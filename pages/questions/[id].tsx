bdf0f8257df20a7a78d2882517c436156b12a2b7
Deepak Narayan <xdpknx@gmail.com>
2022-07-19 10:27:10 +0200

    build: add questions and result routes

---
+++ pages/questions/[id].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const QuestionPage: NextPage = () => {
+  return <div>QuestionPage</div>;
+};
+
+export default QuestionPage;
---
+++ pages/result.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Result: NextPage = () => {
+  return <div>Results</div>;
+};
+
+export default Result;
