d75b785de28f610d2224e1216c444dafe8544992
Deepak Narayan <xdpknx@gmail.com>
2022-07-19 11:07:12 +0200

    feat: implement question page

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -1,7 +1,52 @@
-import type { NextPage } from 'next';
+import { PrismaClient } from '@prisma/client';
+import { GetServerSideProps } from 'next';
+import QuestionCard from '../../components/cards/questioncard/QuestionCard';
+import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
+import { Question } from '../../types';
+import { NextPageWithLayout } from '.././page';
 
-const QuestionPage: NextPage = () => {
-  return <div>QuestionPage</div>;
+interface Props {
+  question: Question;
+}
+const QuestionPage: NextPageWithLayout<Props> = ({ question }) => {
+  return (
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <QuestionCard
+        title={question.title}
+        id={question.id}
+        answers={question.answers}
+      />
+    </section>
+  );
 };
 
 export default QuestionPage;
+
+QuestionPage.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const prisma = new PrismaClient();
+  const question = await prisma.question.findFirst({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      id: true,
+      title: true,
+      answers: true,
+    },
+  });
+  if (!question) {
+    context.res.setHeader('location', '/result');
+    context.res.statusCode = 302;
+    context.res.end();
+  }
+  return {
+    props: {
+      question,
+    },
+  };
+};
