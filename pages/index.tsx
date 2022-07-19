dff9b15334adddcf06e0d4fccc0a98fe9cec521d
Deepak Narayan <xdpknx@gmail.com>
2022-07-19 10:54:02 +0200

    feat: implement index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-import type { NextPage } from 'next';
+import Link from 'next/link';
+import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
+import { NextPageWithLayout } from './page';
 
-const Home: NextPage = () => {
-  return <div>psyNow</div>;
+const Home: NextPageWithLayout = () => {
+  return (
+    <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
+      <h1 className="text-xl font-bold">
+        Welcome to the PsyNow personality test
+      </h1>
+
+      <p>
+        This test works by asking you 3-5 questions . All questions are
+        mandatory. Depending on the answers you will be graded as an Introvert
+        or an Extrovert.
+      </p>
+      <Link href="questions/1" passHref>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 rounded font-semibold w-full my-1">
+          Start Test
+        </button>
+      </Link>
+    </section>
+  );
 };
 
 export default Home;
+
+Home.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
